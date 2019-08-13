70a2a3c2e754cd2e4d078d02ac770823c8b8f2bd
Mark Satterthwaite <mark.satterthwaite@epicgames.com>
2019-08-13 13:59:03 -0400

    Don't remove static const variables when rewriting HLSL.

--- tools/clang/tools/libclang/dxcrewriteunused.cpp
+++ tools/clang/tools/libclang/dxcrewriteunused.cpp
@@ -394,7 +394,9 @@ HRESULT DoRewriteUnused(_In_ DxcLangExtensionsHelper *pHelper,
     if (tuDecl->isImplicit()) continue;
 
     VarDecl* varDecl = dyn_cast_or_null<VarDecl>(tuDecl);
-    if (varDecl != nullptr && varDecl->getFormalLinkage() == clang::Linkage::InternalLinkage) {
+    /* UE-Change-Begin: Don't elide static const variables */
+    if (varDecl != nullptr && varDecl->getStorageClass() != SC_Static) {
+    /* UE-Change-End: Don't elide static const variables */
       unusedGlobals.insert(varDecl);
       if (const RecordType *recordType = varDecl->getType()->getAs<RecordType>()) {
         RecordDecl *recordDecl = recordType->getDecl();
